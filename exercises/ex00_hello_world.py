69abd4e5b87a70ebe4317bef8651f2cb6412ecd3
Charles Larkin <cwflark@email.unc.edu>
2021-08-22 20:54:27 -0400

    Finished Exercise 00! 2nd

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,6 +1,6 @@
-"""My first program for COMP110"""
+"""My first program for COMP110."""
 
 __author__ = "730407702"
 
 print("Hello, World.")
-print("Hello, Charlies World.")
+print("Hello, Charlies World")
